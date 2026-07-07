2dad199f2805067f9d211592389e83db1c33ee7f
tianhangj <tianhangj@sjtu.edu.cn>
2026-07-07 15:37:30 +0800

    fix some errors and add udp associate support

--- code/socks5/socks5.go
+++ code/socks5/socks5.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"bytes"
 	"encoding/binary"
+	"errors"
 	"flag"
 	"fmt"
 	"io"
@@ -19,20 +20,22 @@ func process(conn net.Conn) {
 	defer conn.Close()
 	reader := bufio.NewReader(conn)
 	writer := bufio.NewWriter(conn)
-
-	var buf [512]byte
-	n, err := reader.Read(buf[:])
-	if n == 0 && err != nil {
-		fmt.Println("read from client failed, err:", err)
-		return
-	}
-	ver := buf[0]
-	if ver != 0x05 {
+	ver, err := reader.ReadByte()
+	if ver != 0x05 || err != nil {
 		fmt.Println("socks version not supported")
 		return
 	}
-	nMethods := int(buf[1])
-	methods := buf[2 : 2+nMethods]
+	nMethods, err := reader.ReadByte()
+	if err != nil {
+		fmt.Println("read nMethods failed, err:", err)
+		return
+	}
+	methods := make([]byte, nMethods)
+	n, err := io.ReadFull(reader, methods)
+	if n != int(nMethods) || err != nil {
+		fmt.Println("read methods failed, err:", err)
+		return
+	}
 	supportNoAuth := false
 	for _, method := range methods {
 		if method == 0x00 {
@@ -43,11 +46,6 @@ func process(conn net.Conn) {
 				fmt.Println("write to client failed, err:", err)
 				return
 			}
-			n, err = reader.Read(buf[:])
-			if n == 0 && err != nil {
-				fmt.Println("read from client failed, err:", err)
-				return
-			}
 			break
 		}
 	}
@@ -56,31 +54,63 @@ func process(conn net.Conn) {
 		fmt.Println("no supported auth method")
 		return
 	}
-	
-	bytesBuffer := bytes.NewBuffer(buf[1:n])
-	cmd := bytesBuffer.Next(1)
-	bytesBuffer.Next(1)
-	atyp := bytesBuffer.Next(1)
-	var dspAddr string
-	switch atyp[0] {
-	case 0x01:
-		dspAddr = net.IP(bytesBuffer.Next(net.IPv4len)).String()
-	case 0x03:
-		domainLen := int(bytesBuffer.Next(1)[0])
-		dspAddr = string(bytesBuffer.Next(domainLen))
-	case 0x04:
-		dspAddr = net.IP(bytesBuffer.Next(net.IPv6len)).String()
-	default:
-		fmt.Println("address type not supported")
+	reader.ReadByte()
+	cmd, err := reader.ReadByte()
+	reader.ReadByte()
+	readAddr := func(reader *bufio.Reader) (string, error) {
+		var dspAddr string
+		atyp, err := reader.ReadByte()
+		if err != nil {
+			return "", fmt.Errorf("read atyp failed, err: %v", err)
+		}
+		switch atyp {
+		case 0x01:
+			_dspAddr := make([]byte, net.IPv4len)
+			n, err := io.ReadFull(reader, _dspAddr)
+			if n != net.IPv4len || err != nil {
+				return "", fmt.Errorf("read dspAddr failed, err: %v", err)
+			}
+			dspAddr = net.IP(_dspAddr).String()
+		case 0x03:
+			domainLen, err := reader.ReadByte()
+			fmt.Println("domainLen:", domainLen)
+			if err != nil {
+				return "", fmt.Errorf("read domainLen failed, err: %v", err)
+			}
+			_dspAddr := make([]byte, int(domainLen))
+			n, err := io.ReadFull(reader, _dspAddr)
+			if n != int(domainLen) || err != nil {
+				return "", fmt.Errorf("read dspAddr failed, err: %v", err)
+			}
+			dspAddr = string(_dspAddr)
+			if dspAddr == "0" {
+				dspAddr = "0.0.0.0"
+			}
+		case 0x04:
+			_dspAddr := make([]byte, net.IPv6len)
+			n, err := io.ReadFull(reader, _dspAddr)
+			if n != net.IPv6len || err != nil {
+				return "", fmt.Errorf("read dspAddr failed, err: %v", err)
+			}
+			dspAddr = net.IP(_dspAddr).String()
+		default:
+			fmt.Println("address type not supported")
+			return "", errors.New("address type not supported")
+		}
+		return dspAddr, nil
+	}
+	dspAddr, err := readAddr(reader)
+	if err != nil {
+		fmt.Println(err)
 		return
 	}
 	var dspPort uint16
-	err = binary.Read(bytesBuffer, binary.BigEndian, &dspPort)
+	err = binary.Read(reader, binary.BigEndian, &dspPort)
 	if err != nil {
 		fmt.Println("read port failed, err:", err)
 		return
 	}
-	switch cmd[0] {
+	switch cmd {
 	case 0x01:
 		if dspPort == 0 {
 			fmt.Println("invalid port")
@@ -110,9 +140,9 @@ func process(conn net.Conn) {
 			fmt.Println("flush connection failed, err:", err)
 			return
 		}
-		go io.Copy(serverWriter, reader)
-		go io.Copy(writer, serverReader)
 		done := make(chan error, 2)
+		go forward(reader, serverWriter, done)
+		go forward(serverReader, writer, done)
 		for i := 0; i < 2; i++ {
 			e := <-done
 			if e != nil {
@@ -124,7 +154,99 @@ func process(conn net.Conn) {
 		fmt.Println("bind command not supported")
 		return
 	case 0x03:
-		fmt.Println("UDP associate command not supported")
+		laddr := net.JoinHostPort(dspAddr, fmt.Sprintf("%d", dspPort))
+		laddr = "0.0.0.0:0"
+		fmt.Println("udp associate request to", laddr)
+		udpAddr, err := net.ResolveUDPAddr("udp", laddr)
+		if err != nil {
+			fmt.Println("resolve udp addr failed, err:", err)
+			return
+		}
+		udpConn, err := net.ListenUDP("udp", udpAddr)
+		if err != nil {
+			fmt.Println("listen udp failed, err:", err)
+			return
+		}
+		fmt.Println("udp listen on", udpConn.LocalAddr())
+		defer udpConn.Close()
+		writer.Write([]byte{0x05, 0x00, 0x00})
+		if udpConn.LocalAddr().(*net.UDPAddr).IP.To4() != nil {
+			writer.Write([]byte{0x01})
+			writer.Write(udpConn.LocalAddr().(*net.UDPAddr).IP.To4())
+		} else {
+			writer.Write([]byte{0x04})
+			writer.Write(udpConn.LocalAddr().(*net.UDPAddr).IP.To16())
+		}
+		binary.Write(writer, binary.BigEndian, uint16(udpConn.LocalAddr().(*net.UDPAddr).Port))
+		err = writer.Flush()
+		if err != nil {
+			fmt.Println("flush connection failed, err:", err)
+			return
+		}
+		var firstPacket bool = true
+		var clientUDPAddr net.UDPAddr
+		done := make(chan int)
+		go func() {
+			buf := make([]byte, 65536)
+			for len(done) == 0 {
+				n, addr, err := udpConn.ReadFromUDP(buf)
+				if err != nil {
+					fmt.Println("read udp failed, err:", err)
+					return
+				}
+				if firstPacket {
+					clientUDPAddr = *addr
+					firstPacket = false
+				}
+				if clientUDPAddr.IP.Equal(addr.IP) && clientUDPAddr.Port == addr.Port {
+					if buf[0] != 0x00 || buf[1] != 0x00 || buf[2] != 0x00 {
+						fmt.Println("invalid udp request header, drop")
+						continue
+					}
+					stream := bufio.NewReader(bytes.NewBuffer(buf[3:n]))
+					dspAddr, err := readAddr(stream)
+					if err != nil {
+						fmt.Println("read udp dspAddr failed, err:", err)
+						return
+					}
+					var dspPort uint16
+					err = binary.Read(stream, binary.BigEndian, &dspPort)
+					if err != nil {
+						fmt.Println("read udp dspPort failed, err:", err)
+						return
+					}
+					raddr := net.JoinHostPort(dspAddr, fmt.Sprintf("%d", dspPort))
+					rUDPAddr, _ := net.ResolveUDPAddr("udp", raddr)
+					payload, err := io.ReadAll(stream)
+					udpConn.WriteToUDP(payload, rUDPAddr)
+				} else {
+					var packet bytes.Buffer = bytes.Buffer{}
+					packet.Write([]byte{0x00, 0x00, 0x00})
+					if addr.IP.To4() != nil {
+						packet.Write([]byte{0x01})
+						packet.Write(addr.IP.To4())
+					} else {
+						packet.Write([]byte{0x04})
+						packet.Write(addr.IP.To16())
+					}
+					binary.Write(&packet, binary.BigEndian, uint16(addr.Port))
+					packet.Write(buf[:n])
+					udpConn.WriteToUDP(packet.Bytes(), &clientUDPAddr)
+				}
+			}
+		}()
+		for {
+			buf := make([]byte, 65536)
+			_, err := reader.Read(buf)
+			if err != nil {
+				fmt.Println("read from tcp failed, err:", err)
+				done <- 1
+				udpConn.Close()
+				break
+			}
+		}
+	default:
+		fmt.Println("command not supported")
 		return
 	}
 }
